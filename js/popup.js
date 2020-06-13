631ed9f7d5803838e5ede3cd084a2cf9d280a34b
Ksenya <ksenya-7@yandex.ru>
2020-06-13 20:56:39 +0300

    исправления popup

--- js/popup.js
+++ js/popup.js
@@ -1,38 +1,63 @@
 var popupLink = document.querySelector(".popup-link");
 var popupCallback = document.querySelector(".callback");
-var popupClose = popupCallback.querySelector(".popup-close");
-var popupForm = popupCallback.querySelector(".callback-form");
-var userName = popupCallback.querySelector(".name-user");
-var loginLogin = popupCallback.querySelector(".login-user");
+var popupClose = popupCallback.querySelector("[type=button]");
+var popupForm = popupCallback.querySelector("form");
+var popupSubmit = popupCallback.querySelector("[type=submit]");
+var userName = popupCallback.querySelector("[name=name-user]");
+var loginLogin = popupCallback.querySelector("[name=login]");
+var modal = document.querySelector(".modal");
+var overlay = document.querySelector(".modal-overlay");
 
 var isStorageSupport = true;
-var storage = "";
+var storageName = "";
+var storageLogin = "";
+
+var closeModal = function () {
+  popupCallback.classList.remove("callback-show");
+  popupCallback.classList.remove("callback-error");
+  modal.classList.remove("modal-active");
+};
 
 try {
-  storage = localStorage.getItem("login");
+  storageLogin = localStorage.getItem("login");
+  storageName = localStorage.getItem("name");
 } catch (err) {
   isStorageSupport = false;
 }
 
 popupLink.addEventListener("click", function (evt) {
+  "use strict";
   evt.preventDefault();
   popupCallback.classList.add("callback-show");
-  userName.focus();
-  if (storage) {
-    loginLogin.value = storage;
-    userName.focus();
+  modal.classList.add("modal-active");
+
+  if (storageName && storageLogin) {
+    popupSubmit.focus();
   } else {
-    loginLogin.focus();
+    userName.focus();
   }
 });
 
 popupClose.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popupCallback.classList.remove("callback-show");
-  popupCallback.classList.remove("callback-error");
+  closeModal();
 });
 
-popupForm.addEventListener("submit", function (evt) {
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  closeModal();
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popupCallback.classList.contains("callback-show")) {
+      evt.preventDefault();
+      closeModal();
+    }
+  }
+});
+
+popupForm.addEventListener("submit", function (evt) { //popupForm.onsubmit = function(evt) - это то же самое?
   if (!userName.value || !loginLogin.value) {
     evt.preventDefault();
     popupCallback.classList.remove("callback-error");
@@ -41,16 +66,7 @@ popupForm.addEventListener("submit", function (evt) {
   } else {
     if (isStorageSupport) {
       localStorage.setItem("login", loginLogin.value);
-    }
-  }
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (popupCallback.classList.contains("callback-show")) {
-      evt.preventDefault();
-      popupCallback.classList.remove("callback-show");
-      popupCallback.classList.remove("callback-error");
+      localStorage.setItem("name", userName.value);
     }
   }
 });
